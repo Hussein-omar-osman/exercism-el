339b95462694c1454a78ddd61a0a35035ca0da99
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2022-08-08 20:03:05 +0300

    creating conversion funcs

--- kitchen_calculator/lib/kitchen_calculator.ex
+++ kitchen_calculator/lib/kitchen_calculator.ex
@@ -3,16 +3,36 @@ defmodule KitchenCalculator do
   Documentation for `KitchenCalculator`.
   """
 
-  @doc """
-  Hello world.
+  def get_volume({_, num}) do
+    # Please implement the get_volume/1 function
+    num
+  end
 
-  ## Examples
+  def to_milliliter({us, num}) do
+    # Please implement the to_milliliter/1 functions
+    case us do
+      :cup -> {:milliliter, num * 240}
+      :fluid_ounce -> {:milliliter, num * 30}
+      :teaspoon -> {:milliliter, num * 5}
+      :tablespoon -> {:milliliter, num * 15}
+      :milliliter -> {:milliliter, num}
+    end
+  end
 
-      iex> KitchenCalculator.hello()
-      :world
+  def from_milliliter({_, num}, unit) do
+    # Please implement the from_milliliter/2 functions
+    case unit do
+      :cup -> {unit, num / 240}
+      :fluid_ounce -> {unit, num / 30}
+      :teaspoon -> {unit, num / 5}
+      :tablespoon -> {unit, num / 15}
+      :milliliter -> {unit, num}
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def convert(volume_pair, unit) do
+    # Please implement the convert/2 function
+    to_ml = to_milliliter(volume_pair)
+    from_milliliter(to_ml, unit)
   end
 end
