8ed8dac91fb5db5f8baeff5eb7f2e61f3717802e
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2022-08-16 01:35:14 +0300

    compare func with guards and multiple clause functions

--- guessing/lib/guessing.ex
+++ guessing/lib/guessing.ex
@@ -2,17 +2,14 @@ defmodule Guessing do
   @moduledoc """
   Documentation for `Guessing`.
   """
+  def compare(secret_number, guess \\ 0)
+  def compare(_, guess) when guess == 0, do: "Make a guess"
+  def compare(_, guess) when guess == :no_guess, do: "Make a guess"
+  def compare(secret_number, guess) when secret_number == guess, do: "Correct"
 
-  @doc """
-  Hello world.
+  def compare(secret_number, guess) when guess == secret_number - 1 or guess == secret_number + 1,
+    do: "So close"
 
-  ## Examples
-
-      iex> Guessing.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
 end
