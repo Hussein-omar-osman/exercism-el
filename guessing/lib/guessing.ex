2566b52a9ac94647e7d333ad797a668caae5fef4
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2022-08-23 01:07:36 +0300

    Guessing init

---
+++ guessing/lib/guessing.ex
@@ -0,0 +1,18 @@
+defmodule Guessing do
+  @moduledoc """
+  Documentation for `Guessing`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Guessing.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
