1d4dc42c1fa3f8ae31fa11ab2e631432d16a42dc
Hussein Omar <husseinomar6190@gmail.com>
2022-12-29 12:13:02 +0300

    initial commit

---
+++ in_exercices/lib/in_exercices.ex
@@ -0,0 +1,18 @@
+defmodule InExercices do
+  @moduledoc """
+  Documentation for `InExercices`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> InExercices.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ in_exercices/test/in_exercices_test.exs
@@ -0,0 +1,8 @@
+defmodule InExercicesTest do
+  use ExUnit.Case
+  doctest InExercices
+
+  test "greets the world" do
+    assert InExercices.hello() == :world
+  end
+end
