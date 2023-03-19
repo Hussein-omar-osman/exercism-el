7ec98524ef80cb0eef60457a1db6e2cee5ccf1b0
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2023-03-19 18:49:45 +0300

    finished to_label func

--- log_level/lib/log_level.ex
+++ log_level/lib/log_level.ex
@@ -1,18 +1,26 @@
 defmodule LogLevel do
-  @moduledoc """
-  Documentation for `LogLevel`.
-  """
+  @spec to_label(any, any) :: :debug | :error | :fatal | :info | :trace | :unknown | :warning
+  def to_label(level, legacy) do
+    # Please implement the to_label/2 function
+    cond do
+       level == 0 ->
+          cond do
+            legacy -> :unknown
+            true -> :trace
+          end
+       level == 1 -> :debug
+       level == 2 -> :info
+       level == 3 -> :warning
+       level == 4 -> :error
+       level == 5 ->
+        cond do
+          legacy -> :unknown
+          true -> :fatal
+        end
+       true -> :unknown
+     end
 
-  @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> LogLevel.hello()
-      :world
-
-  """
-  def hello do
-    :world
   end
+
 end
